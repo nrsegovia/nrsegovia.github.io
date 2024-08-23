3e56be6512892d82413cb4979906e04638758555
Nicolas Rodriguez-Segovia <n.rsegovia@adfa.edu.au>
2024-08-23 18:27:28 +1000

    Trying to make articles work again

--- content/news/news1 copy 2.md
+++ content/news/news1 copy 2.md
@@ -1,4 +1,5 @@
 Title: News Article 3
 Date: 2024-04-26
+Template: article
 
 This is the content of the first news article.
--- pelicanconf.py
+++ pelicanconf.py
@@ -36,6 +36,8 @@ PAGE_ORDER_BY = 'date'
 PAGE_EXCLUDES = []
 STATIC_PATHS = ['images']
 ARTICLE_PATHS = ['news']
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{slug}.html'
 
 TEMPLATE_PAGES = {'publications.html': 'publications.html',
                   'cv.html': 'cv.html',
